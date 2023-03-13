aa94297c85fec870c5750007d341242735dc8372
Slonexxx <21moriarty21@mail.ru>
2023-03-13 14:17:19 +0600

    new application for MyWarehouse

--- resources/views/main/ZCloseShift.blade.php
+++ resources/views/main/ZCloseShift.blade.php
@@ -3,14 +3,8 @@
 @section('content')
 
     <div class="p-4 mx-1 mt-1 bg-white rounded py-3">
-
-        <div class="row gradient rounded p-2 pb-2" style="margin-top: -1rem">
-            <div class="col-10" style="margin-top: 1.2rem"> <span class="text-black" style="font-size: 20px"> Смена &#8594; Z-отчёт  </span> </div>
-            <div class="col-2 text-center">
-                <img src="https://dev.smarttis.kz/Config/logo.png" width="50%"  alt="">
-                <div style="font-size: 11px; margin-top: 8px"> <b>Топ партнёр сервиса МойСклад</b> </div>
-            </div>
-        </div>
+        @include('div.TopServicePartner')
+        <script> NAME_HEADER_TOP_SERVICE("Смена → Z-отчёт") </script>
 
         @isset($message_good)
 
